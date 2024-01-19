93d0fefaad03a487c1040ae24934e04c52e47250
Florian Knoop <florian.knoop@liu.se>
2024-01-19 18:07:22 +0100

    predictions2tdep | allow multiple input files

--- mlfftools/scripts/sokrates_predictions2tdep.py
+++ mlfftools/scripts/sokrates_predictions2tdep.py
@@ -1,16 +1,14 @@
 #! /usr/bin/env python3
 
 from pathlib import Path
-from typing import List
 
+from typing import List
 import numpy as np
 import typer
-from ase.io import read
 from ase.units import GPa
 from rich import print as echo
 from rich.progress import track
 from tdeptools.scripts.tdep_parse_output import (
-    dimensions,
     keys,
     write_infiles,
     write_meta,
@@ -20,31 +18,33 @@ app = typer.Typer(pretty_exceptions_show_locals=False)
 
 
 @app.command()
-def main(file: Path):
+def main(files: List[Path]):
     """Convert trajectory files to MLFF input as npz file"""
     import xarray as xr
 
-    echo(f"Convert predictions in {file} to TDEP input")
+    echo(f"Convert predictions in {files} to TDEP input")
 
-    ds = xr.load_dataset(file)
-
-    echo(ds)
     rows = []
-    for ii, time in enumerate(track(ds["time"])):
+    for file in files:
+        echo(f"... open '{file}'")
+        ds = xr.load_dataset(file)
+        echo(ds)
 
-        ds_step = ds.sel(time=time)  # dataset for this step
+        for ii, time in enumerate(track(ds["time"])):
 
-        s = ds_step[keys.stress].data / GPa
-        _stress = np.array([s[0, 0], s[1, 1], s[2, 2], s[2, 1], s[2, 0], s[1, 0]])
-        _pressure = ds_step[keys.pressure].data.squeeze() / GPa
+            ds_step = ds.sel(time=time)  # dataset for this step
 
-        row = {key: ds_step[key].data.squeeze() for key in ds_step.data_vars}
-        row[keys.stress] = _stress
-        row[keys.pressure] = _pressure
-        rows.append(row)
+            s = ds_step[keys.stress].data / GPa
+            _stress = np.array([s[0, 0], s[1, 1], s[2, 2], s[2, 1], s[2, 0], s[1, 0]])
+            _pressure = ds_step[keys.pressure].data.squeeze() / GPa
+
+            row = {key: ds_step[key].data.squeeze() for key in ds_step.data_vars}
+            row[keys.stress] = _stress
+            row[keys.pressure] = _pressure
+            rows.append(row)
 
     write_infiles(rows)
-    write_meta(n_atoms=len(ds.atom), n_samples=len(ds.time))
+    write_meta(n_atoms=len(ds.atom), n_samples=len(rows))
 
 
 if __name__ == "__main__":
